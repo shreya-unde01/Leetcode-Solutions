d6aeea9a1e1e845fd4b567300b64bb98674ef656
shreya-unde01 <shreyaunde71@gmail.com>
2026-08-11 15:07:07 +0530

    Time: 14 ms (90.9%), Space: 46.2 MB (73.43%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+
+        int[] dp = new int[amount + 1];
+
+        // Fill with a value greater than any possible answer
+        for (int i = 1; i <= amount; i++) {
+            dp[i] = amount + 1;
+        }
+
+        dp[0] = 0;
+
+        for (int i = 1; i <= amount; i++) {
+
+            for (int coin : coins) {
+
+                if (coin <= i) {
+                    dp[i] = Math.min(dp[i],
+                                     dp[i - coin] + 1);
+                }
+            }
+        }
+
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+}
